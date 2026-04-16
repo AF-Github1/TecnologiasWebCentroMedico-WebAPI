4e973a8f993ccbe11cfc339b30176b302f016670
Antonio Freitas <133685290+AF-Github1@users.noreply.github.com>
2026-04-16 20:04:54 +0000

    a

--- js/translator.js
+++ js/translator.js
@@ -2,5 +2,9 @@ function googleTranslateElementInit() {
         new google.translate.TranslateElement(
             {pagelanguage: 'pt'},
             'google_translate_element'
+
+
         );
 }
+
+//commitchange
